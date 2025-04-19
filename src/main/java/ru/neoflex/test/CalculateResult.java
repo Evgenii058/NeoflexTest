5d4ad42ec51b6fecb3883e3f3094ded85cc69a5a
Evgenii058 <duomemory@mail.ru>
2025-04-19 19:07:41 +0300

    Rename package

--- src/main/java/ru/neoflex/test/CalculateResult.java
+++ src/main/java/ru/neoflex/test/CalculateResult.java
@@ -1,3 +1,3 @@
-package com.example.restservice;
+package ru.neoflex.test;
 
 public record CalculateResult(String content) { }
--- src/main/java/ru/neoflex/test/NeoflexTestApplication.java
+++ src/main/java/ru/neoflex/test/NeoflexTestApplication.java
@@ -1,4 +1,4 @@
-package com.example.restservice;
+package ru.neoflex.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
