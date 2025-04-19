9233ee584a37f0670d4e65adfc909886f7aae6b1
Evgenii058 <duomemory@mail.ru>
2025-04-19 18:55:32 +0300

    Remove test

--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -37,13 +37,6 @@ public class GreetingControllerTests {
 	private MockMvc mockMvc;
 
 	@Test
-	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
-	}
-
-	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
 		this.mockMvc.perform(get("/Calculate").param("averageSalary", "200").param("vacationDay", "2"))
